421114103eb7a251445af29f80e48b60a9c67acb
lirugo <roostik21@gmail.com>
2018-05-31 11:51:08 +0300

    php artisan make:auth migrate install yarn yarn add vuex npm install XD) npm run dev

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,6 +9,8 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import Vuex from 'vuex'
+Vue.use(vuex);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
